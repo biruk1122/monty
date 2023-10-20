ce08ee4378cd6c0593f6f188c45d7e8c7a24f0e9
biruk1122 <birukfikadu29@gmail.com>
2023-10-20 01:29:36 -0700

    updated

--- push_stack.c
+++ push_stack.c
@@ -1,37 +1,10 @@
 #include "monty.h"
-<<<<<<< HEAD
 
-
-void push_op(stack_t **stack)/*unsigned int line_number*/
-=======
 /**
  * push_op - pushing a value
  * @stack: stack
  * @line_number: line number
  */
-/*void push_op(stack_t **stack, unsigned int line_number)
->>>>>>> 28f109157040416f2fea5fde4b736aeed0d167e2
-{
-	stack_t *new_node;
-	(void)line_number;
-
-	new_node = malloc(sizeof(stack_t));
-	if (new_node == NULL)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
-		free_stack(*stack);
-		exit(EXIT_FAILURE);
-	}
-	new_node->n = globals.value;
-	new_node->prev = NULL;
-	new_node->next = *stack;
-
-	if (*stack != NULL)
-	{
-		(*stack)->prev = new_node;
-		*stack = new_node;
-	}
-}*/
 void push_op(stack_t **stack, unsigned int line_number)
 {
 	stack_t *new_node;
@@ -52,17 +25,12 @@ void push_op(stack_t **stack, unsigned int line_number)
 	*stack = new_node;
 }
 
-<<<<<<< HEAD
-
-void pall_op(stack_t **stack)/*unsigned int line_number*/
-=======
 /**
  * pall_op - pall
  * @stack: stack
  * @line_number: line
  */
 void pall_op(stack_t **stack, unsigned int line_number)
->>>>>>> 28f109157040416f2fea5fde4b736aeed0d167e2
 {
 	stack_t *current = *stack;
 
@@ -96,22 +64,15 @@ void pint_op(stack_t **stack, unsigned int line_number)
 void pop_op(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp;
-<<<<<<< HEAD
 	temp = *stack;
-=======
 
->>>>>>> 28f109157040416f2fea5fde4b736aeed0d167e2
 	if (*stack == NULL)
 	{
 		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-<<<<<<< HEAD
-	/*stack_t *temp = *stack;*/
-=======
 
 	temp = *stack;
->>>>>>> 28f109157040416f2fea5fde4b736aeed0d167e2
 	*stack = (*stack)->next;
 	if (*stack != NULL)
 	{
@@ -127,28 +88,20 @@ void pop_op(stack_t **stack, unsigned int line_number)
 
 void swap_op(stack_t **stack, unsigned int line_number)
 {
-<<<<<<< HEAD
 	stack_t *second;
 	stack_t *first;
 	first = *stack;
 	second = first->next;
-=======
-	stack_t *first;
-	stack_t *second;
+	/*stack_t *first;*/
+	/*stack_t *second;*/
 
->>>>>>> 28f109157040416f2fea5fde4b736aeed0d167e2
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-<<<<<<< HEAD
-	/*stack_t *first = *stack;*/
-	/*stack_t *second = first->next;*/
-=======
 	first = *stack;
 	second = first->next;
->>>>>>> 28f109157040416f2fea5fde4b736aeed0d167e2
 	first->next = second->next;
 	second->prev = NULL;
 	if (first->next != NULL)
